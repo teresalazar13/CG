6c4fb70eb46de3e289bab7c67857474287d7d127
Teresa Margarida Baptista Cabral De Carvalho Salazar <tmsalazar@student.dei.uc.pt>
2018-06-09 17:47:54 +0100

    Corrects rain.

--- rain.cpp
+++ rain.cpp
@@ -2,25 +2,20 @@
 
 float slowdown = 2.0;
 float velocity = 0.0;
-float zoom = -40.0;
 int loop;
 
 // Initialize/Reset Particles - give them their attributes
 void Rain::setup_particle(int i) {
   par_sys[i].alive = true;
   par_sys[i].life = 1.0;
-  par_sys[i].fade = float(rand()%100)/1000.0f+0.003f;
+  par_sys[i].fade = float(rand() % 100)/ 1000.0f + 0.003f;
 
   par_sys[i].xpos = 5.0;
-  par_sys[i].ypos = 5.0;
+  par_sys[i].ypos = 20.0;
   par_sys[i].zpos = 5.0;
 
-  par_sys[i].red = 0.5;
-  par_sys[i].green = 0.5;
-  par_sys[i].blue = 1.0;
-
   par_sys[i].vel = velocity;
-  par_sys[i].gravity = -0.8;//-0.8;
+  par_sys[i].gravity = -0.8;
 }
 
 void Rain::init_particles() {
@@ -33,16 +28,19 @@ void Rain::render_rain() {
   float x, y, z;
   for (loop = 0; loop < 1000; loop=loop+2) {
     if (par_sys[loop].alive == true) {
+
       x = par_sys[loop].xpos;
       y = par_sys[loop].ypos;
-      z = par_sys[loop].zpos + zoom;
+      z = par_sys[loop].zpos;
 
       // Draw particles
-      glColor3f(0, 0, 1.0);
+      glColor4f(0, 0, 1, par_sys[loop].life);
       glBegin(GL_LINES);
         glVertex3f(x, y, z);
-        glVertex3f(x, y+5, z);
+        glVertex3f(x, y + 0.1, z);
       glEnd();
+
+      glColor4f(1, 1, 1, 1);
 
       // Update values
       //Move
@@ -52,14 +50,13 @@ void Rain::render_rain() {
       // Decay
       par_sys[loop].life -= par_sys[loop].fade;
 
-      if (par_sys[loop].ypos <= -10) {
+      if (par_sys[loop].ypos <= -20) {
         par_sys[loop].life = -1.0;
       }
       //Revive
-      if (par_sys[loop].life < 0.0) {
+      if (par_sys[loop].life < -1.0) {
         setup_particle(loop);
       }
     }
-    glColor4f(1, 1, 1, 1);
   }
 }
